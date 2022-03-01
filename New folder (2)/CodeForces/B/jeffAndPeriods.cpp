2748c5e1cfa5b88b0844c540a7abef253058f398
AMBUJ KUMAR <90747841+fantom787@users.noreply.github.com>
2022-03-02 01:08:59 +0530

    2nd March 2022

---
+++ New folder (2)/CodeForces/B/jeffAndPeriods.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.1415926535;
+#define endl "\n";
+#define ll long long int
+#define sort(x) sort(x.begin(), x.end());
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    return 0;
+}
