e56cc4b87f8d939b4d9d1007905d3adaf714dcf0
AMBUJ KUMAR <90747841+fantom787@users.noreply.github.com>
2022-03-14 19:40:58 +0530

    14th Mar 2022

---
+++ New folder (2)/CodeForces/B/xeniaAndSpies.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.1415926535;
+#define endl "\n";
+#define ll long long int
+#define all(s) s.begin(), s.end()
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    return 0;
+}
