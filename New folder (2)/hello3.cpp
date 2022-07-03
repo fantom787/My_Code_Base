98e3afc28871c42efde06008b22e568fcd265aad
AMBUJ KUMAR <90747841+fantom787@users.noreply.github.com>
2022-07-03 15:41:46 +0530

    3 Jul 2022

--- New folder (2)/hello3.cpp
+++ New folder (2)/hello3.cpp
@@ -1,299 +1,141 @@
-/*
-    Author: Ambuj Kumar(fantom787)
-    If it works, don't touch it.
-----------------------All Links -------------------------
- Youtube:  https://www.youtube.com/c/Fantom7877/videos
- Linkedin: https://www.linkedin.com/in/ambuj-kumar-88b614203/
- Codeforces: https://codeforces.com/profile/fantom787
- Codechef: https://www.codechef.com/users/ambuj787
-*/
+/* Priyansh Agarwal*/
 
-// Pragmas
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2")
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
 
-// header files
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> // for pbds
-#include <ext/pb_ds/tree_policy.hpp>
-
-// namespace
 using namespace std;
 using namespace chrono;
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
 
-// ordered set
-template <class T, class cmp = less<T>>
-using ordered_set = tree<T, null_type, cmp, rb_tree_tag, tree_order_statistics_node_update>;
-
-// ordered map
-template <class key, class value, class cmp = less<key>>
-using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
-// find_by_order(k)  returns iterator to kth element starting from 0;
-// order_of_key(k) returns count of elements strictly smaller than k;
-
-template <class T>
-using min_heap = priority_queue<T, vector<T>, greater<T>>;
-
-// Constants
-#define PI 3.1415926535
-#define INF 4e18
-#define EPS 1e-9
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define MOD 1000000007
-
-// Aliases
-using ll = long long int;
-using ull = unsigned long long int;
-using ld = long double;
-
-// debug
-#define debug(x)       \
-    cout << #x << " "; \
-    _print(x);         \
-    cout << endl;
-void _print(ll t)
-{
-    cout << t;
-}
-void _print(int t) { cout << t; }
-void _print(string t) { cout << t; }
-void _print(char t) { cout << t; }
-void _print(ld t) { cout << t; }
-void _print(double t) { cout << t; }
-void _print(ull t) { cout << t; }
-
-// macros
-// #define endl "\n";
-#define all(s) s.begin(), s.end()
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
 #define pb push_back
-#define eb emplace_back
-#define ppc __builtin_popcount
-#define parity(x) __builtin_parity(x) // gives true for odd and false for even
-#define ppcll __builtin_popcountll
-#define msb(x) 63 - __builtin_clzll(x) // gives the most significant bit of the number
-#define sz(x) (int)x.size()
-#define F first
-#define S second
-#define int long long
-#define getunique(v)                                  \
-    {                                                 \
-        sort(v.begin(), v.end());                     \
-        v.erase(unique(v.begin(), v.end()), v.end()); \
-    }
-#define kickstart(x)                 \
-    {                                \
-        cout << "Case #" << x << ":" \
-             << " ";                 \
-    }
-// custom hash map
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        // Credits: https://codeforces.com/blog/entry/62393
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
 
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-template <typename T1, typename T2> // Key should be integer type
-using safe_map = unordered_map<T1, T2, custom_hash>;
+#ifdef Priyansh31dec
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
 
-// Operator overloads
-template <typename T1, typename T2> // cin >> pair<T1, T2>
-istream &operator>>(istream &istream, pair<T1, T2> &p)
-{
-    return (istream >> p.first >> p.second);
-}
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
-template <typename T> // cin >> vector<T>
-istream &operator>>(istream &istream, vector<T> &v)
-{
-    for (auto &it : v)
-        cin >> it;
-    return istream;
-}
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
-template <typename T1, typename T2> // cout << pair<T1, T2>
-ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
-{
-    return (ostream << p.first << " " << p.second);
-}
-template <typename T> // cout << vector<T>
-ostream &operator<<(ostream &ostream, const vector<T> &c)
-{
-    for (auto &it : c)
-        cout << it << " ";
-    return ostream;
-}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
-// Mathematical functions
-int gcd(int a, int b)
-{
-    while (b)
-    {
-        a %= b;
-        swap(a, b);
-    }
-    return a;
-}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
-int gcdX(int a, int b, int &x, int &y) // gcd extended
-{
-    x = 1, y = 0;
-    int x1 = 0, y1 = 1, a1 = a, b1 = b;
-    while (b1)
-    {
-        int q = a1 / b1;
-        tie(x, x1) = make_tuple(x1, x - q * x1);
-        tie(y, y1) = make_tuple(y1, y - q * y1);
-        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
-    }
-    return a1;
+ll helper(vector<ll> p, vector<ll> n, int k){
+	debug(p)
+	debug(n)
+	ll ans1 = 0;
+	while(sz(p) > 0){
+		ans1 += 2LL * p.back();
+		int curr = sz(p);
+		for(int i = 0; i < min(curr, k); i++){
+			p.ppb();
+		}
+	}
+	ll ans2 = 0;
+	if(sz(n) > 0){
+		ans2 += n.back();
+		int curr = sz(n);
+		for(int i = 0; i < min(curr, k); i++){
+			n.ppb();
+		}
+		while(sz(n) > 0){
+			ans2 += 2LL * n.back();
+			curr = sz(n);
+			for(int i = 0; i < min(curr, k); i++){
+				n.ppb();
+			}
+		}	
+	}
+	debug(mp(ans1, ans2))
+	return ans1 + ans2;
 }
-
-int lcm(int a, int b)
-{
-    return ((ll)a * b) / gcd(a, b);
+void solve(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n, k;
+		cin >> n >> k;
+		vector<ll> arr(n);
+		for(int i = 0; i < n; i++){
+			cin >> arr[i];
+		}
+		vector<ll> pos, neg;
+		for(int i = 0; i < n; i++){
+			if(arr[i] > 0LL){
+				pos.pb(arr[i]);
+			}
+			if(arr[i] < 0LL){
+				neg.pb(-arr[i]);
+			}
+		}
+		sort(all(pos));
+		sort(all(neg));
+		cout << min(helper(pos, neg, k), helper(neg, pos, k)) << nline;
+	}
 }
-// modular exponentiation
-int modpow(int x, int n, int m = MOD)
-{
-    if (x == 0 && n == 0)
-        return 0; // undefined case
-    int res = 1;
-    while (n)
-    {
-        if (n & 1)
-        {
-            res = ((res % m) * (x % m)) % m;
-        }
-        x = ((x % m) * (x % m)) % m;
-        n >>= 1;
-    }
-    return res;
-}
-
-int modinv(int x, int m = MOD)
-{
-    return modpow(x, m - 2, m);
-}
-
-/*--------------- Seive -----------------------*/
-// Get All The Divisors Of That Number
-vector<int> getdiv(int n)
-{
-    vector<int> ans;
-    for (int i = 1; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            ans.pb(i);
-            ans.pb(n / i);
-        }
-    }
-    return ans;
-}
-// to get the prime factors of that number
-vector<int> getprimefac(int n)
-{
-    vector<int> ans;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            while (n % i == 0)
-            {
-                n /= i;
-                ans.pb(i);
-            }
-        }
-    }
-    return ans;
-}
-// get instant prime
-vector<ll> sieve(int n)
-{
-    int *arr = new int[n + 1]();
-    vector<ll> vect;
-    for (int i = 2; i <= n; i++)
-        if (arr[i] == 0)
-        {
-            vect.push_back(i);
-            for (int j = i * i; j <= n; j += i)
-                arr[j] = 1;
-        }
-    return vect;
-}
-// to invert a binary string
-void invert(string &s)
-{
-    int n = sz(s);
-    for (int i = 0; i < n; i++)
-    {
-        s[i] ^= '0' ^ '1';
-    }
-}
-
-// display a vector
-void display(vector<int> a)
-{
-    cout << "Displaying Vector:" << endl;
-    cout << a << endl;
-}
-
-// Flags to use: -std=c++17 -O2 -DLOCAL_PROJECT -Wshadow -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined
-/*/-----------------------------Code begins----------------------------------/*/
-void solve(int testcase)
-{
-    // kickstart(testcase);
-}
-/* stuff you should look for
- * sbse pehle question dobara padho sir
- * Lower_bound  -->Lower_Bound of X ---> element >= x in the set
- * Upper_bound  -->Upper_Bound of X ---> element > x in the set
- * lambda fn is made using {auto name_of_the_function = [&](jo pass krna hai wo){fn body};
- * when u are not able to decide which one to remove then the answer is simply iterate and find the max/min answer for each index
- * if u r multiplying and u have to find equal multipy then u can take 1st and last everytime
- * if u have to make array increasing by adding 1 to subarray then sum of diffrences(which have to increased) is the answer
- * nlog(log(n)) bhi soch lo sir like jha multiples ka case aya wha pe seive of erathosthenisis ka concept lga do
- * cool hoja bsdk
- * jha 1 bdi value aur 1 choti value chahiye wha 2 pointer lga do sir
-    aur choti value wale pointer ko increase krte rehna jb testcase na meet ho to
- * 0 0 n  try kr lo if multiple ans ka case hai to ya isse similar kuch hai to
- * i>j wale sare chahiye to prefix wala lga do sir jisme curr ko calc kro prev se and fir usko map kr do
- * a+b = a^b + 2*a&b
- * a+b = a|b + a&b
- * special cases (n=1?)/ odd/even index
- * sir square wala bhi soch lo
- * consecutive elements ka scene hai to genedy apraoch lga do sir
- * follow the basics koi nya try kr rha hai toh uske primitive try kr
- * XOR --> ALWAYS TRY 45132
- */
-int32_t main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cout << fixed << setprecision(25);
-    cerr << fixed << setprecision(10);
-    auto start = std::chrono::high_resolution_clock::now();
-    int n = 1;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        solve(i);
-    }
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
-    // cerr << "Time taken : " << ((long double)duration.count())/((long double) 1e9) <<"s "<< endl;
-    return 0;
+	
+int main() {
+#ifdef Priyansh31dec
+	freopen("Error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	solve();
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef Priyansh31dec
+	cerr << "Time: " << duration . count() / 1000 << endl;
+#endif
 }
