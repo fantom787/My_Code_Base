00f0e266d531d8465b14f3ffab6d8e0dd8ed5881
AMBUJ KUMAR <90747841+fantom787@users.noreply.github.com>
2022-07-25 15:25:59 +0530

    25 Jul 2022

--- New folder (2)/new.cpp
+++ New folder (2)/new.cpp
@@ -1,317 +1,328 @@
-/*
-    Author: Ambuj Kumar(fantom787)
-    If it works, don't touch it.
-----------------------All Links -------------------------
- Youtube:  https://www.youtube.com/c/Fantom7877/videos
- Linkedin: https://www.linkedin.com/in/ambuj-kumar-88b614203/
- Codeforces: https://codeforces.com/profile/fantom787
- Codechef: https://www.codechef.com/users/ambuj787
-*/
-
-// Pragmas
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2")
-
-// header files
+/**
+ *    author:  tourist
+ *    created: 20.04.2019 21:47:56
+ **/
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> // for pbds
-#include <ext/pb_ds/tree_policy.hpp>
 
-// namespace
 using namespace std;
-using namespace chrono;
-using namespace __gnu_pbds;
 
-/* ordered set*/
-template <class T, class cmp = less<T>>
-using ordered_set = tree<T, null_type, cmp, rb_tree_tag, tree_order_statistics_node_update>;
-
-/* ordered map*/
-template <class key, class value, class cmp = less<key>>
-using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
-/* find_by_order(k)  returns iterator to kth element starting from 0;
- * order_of_key(k) returns count of elements strictly smaller than k;*/
-
-/* min heap*/
-template <class T>
-using min_heap = priority_queue<T, vector<T>, greater<T>>;
-
-// Constants
-#define PI 3.1415926535
-#define INF 4e18
-#define EPS 1e-9
-#define MOD 1000000007
-
-// Aliases
-using ll = long long int;
-using ull = unsigned long long int;
-using ld = long double;
-
-// debug
-#define debug(x)       \
-    cout << #x << " "; \
-    _print(x);         \
-    cout << endl;
-void _print(ll t)
+string to_string(string s)
 {
-    cout << t;
+    return '"' + s + '"';
 }
-void _print(int t) { cout << t; }
-void _print(string t) { cout << t; }
-void _print(char t) { cout << t; }
-void _print(ld t) { cout << t; }
-void _print(double t) { cout << t; }
-void _print(ull t) { cout << t; }
 
-// macros
-// #define endl "\n";
-#define all(s) s.begin(), s.end()
-#define pb push_back
-#define eb emplace_back
-#define ppc __builtin_popcount
-#define parity(x) __builtin_parity(x) // gives true for odd and false for even
-#define ppcll __builtin_popcountll
-#define msb(x) 63 - __builtin_clzll(x) // gives the most significant bit of the number
-#define sz(x) (int)x.size()
-#define F first
-#define S second
-#define int long long
-#define getunique(v)                                  \
-    {                                                 \
-        sort(v.begin(), v.end());                     \
-        v.erase(unique(v.begin(), v.end()), v.end()); \
-    }
-#define kickstart(x)                 \
-    {                                \
-        cout << "Case #" << x << ":" \
-             << " ";                 \
-    }
-// custom hash map
-struct custom_hash
+string to_string(const char *s)
 {
-    static uint64_t splitmix64(uint64_t x)
+    return to_string((string)s);
+}
+
+string to_string(bool b)
+{
+    return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p)
+{
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v)
+{
+    bool first = true;
+    string res = "{";
+    for (const auto &x : v)
     {
-        // Credits: https://codeforces.com/blog/entry/62393
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-template <typename T1, typename T2> // Key should be integer type
-using safe_map = unordered_map<T1, T2, custom_hash>;
-
-// Operator overloads
-template <typename T1, typename T2> // cin >> pair<T1, T2>
-istream &operator>>(istream &istream, pair<T1, T2> &p)
-{
-    return (istream >> p.first >> p.second);
-}
-
-template <typename T> // cin >> vector<T>
-istream &operator>>(istream &istream, vector<T> &v)
-{
-    for (auto &it : v)
-        cin >> it;
-    return istream;
-}
-
-template <typename T1, typename T2> // cout << pair<T1, T2>
-ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
-{
-    return (ostream << p.first << " " << p.second);
-}
-template <typename T> // cout << vector<T>
-ostream &operator<<(ostream &ostream, const vector<T> &c)
-{
-    for (auto &it : c)
-        cout << it << " ";
-    return ostream;
-}
-
-// Mathematical functions
-int gcd(int a, int b)
-{
-    while (b)
-    {
-        a %= b;
-        swap(a, b);
-    }
-    return a;
-}
-
-int gcdX(int a, int b, int &x, int &y) // gcd extended
-{
-    x = 1, y = 0;
-    int x1 = 0, y1 = 1, a1 = a, b1 = b;
-    while (b1)
-    {
-        int q = a1 / b1;
-        tie(x, x1) = make_tuple(x1, x - q * x1);
-        tie(y, y1) = make_tuple(y1, y - q * y1);
-        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
-    }
-    return a1;
-}
-
-int lcm(int a, int b)
-{
-    return ((ll)a * b) / gcd(a, b);
-}
-// modular exponentiation
-int modpow(int x, int n, int m = MOD)
-{
-    if (x == 0 && n == 0)
-        return 0; // undefined case
-    int res = 1;
-    while (n)
-    {
-        if (n & 1)
+        if (!first)
         {
-            res = ((res % m) * (x % m)) % m;
+            res += ", ";
         }
-        x = ((x % m) * (x % m)) % m;
-        n >>= 1;
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T)
+{
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+template <typename T>
+T inverse(T a, T m)
+{
+    T u = 0, v = 1;
+    while (a != 0)
+    {
+        T t = m / a;
+        m -= t * a;
+        swap(a, m);
+        u -= t * v;
+        swap(u, v);
+    }
+    assert(m == 1);
+    return u;
+}
+
+template <typename T>
+class Modular
+{
+public:
+    using Type = typename decay<decltype(T::value)>::type;
+
+    constexpr Modular() : value() {}
+    template <typename U>
+    Modular(const U &x)
+    {
+        value = normalize(x);
+    }
+
+    template <typename U>
+    static Type normalize(const U &x)
+    {
+        Type v;
+        if (-mod() <= x && x < mod())
+            v = static_cast<Type>(x);
+        else
+            v = static_cast<Type>(x % mod());
+        if (v < 0)
+            v += mod();
+        return v;
+    }
+
+    const Type &operator()() const { return value; }
+    template <typename U>
+    explicit operator U() const { return static_cast<U>(value); }
+    constexpr static Type mod() { return T::value; }
+
+    Modular &operator+=(const Modular &other)
+    {
+        if ((value += other.value) >= mod())
+            value -= mod();
+        return *this;
+    }
+    Modular &operator-=(const Modular &other)
+    {
+        if ((value -= other.value) < 0)
+            value += mod();
+        return *this;
+    }
+    template <typename U>
+    Modular &operator+=(const U &other) { return *this += Modular(other); }
+    template <typename U>
+    Modular &operator-=(const U &other) { return *this -= Modular(other); }
+    Modular &operator++() { return *this += 1; }
+    Modular &operator--() { return *this -= 1; }
+    Modular operator++(int)
+    {
+        Modular result(*this);
+        *this += 1;
+        return result;
+    }
+    Modular operator--(int)
+    {
+        Modular result(*this);
+        *this -= 1;
+        return result;
+    }
+    Modular operator-() const { return Modular(-value); }
+
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type &operator*=(const Modular &rhs)
+    {
+#ifdef _WIN32
+        uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+        uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+        asm(
+            "divl %4; \n\t"
+            : "=a"(d), "=d"(m)
+            : "d"(xh), "a"(xl), "r"(mod()));
+        value = m;
+#else
+        value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+#endif
+        return *this;
+    }
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type &operator*=(const Modular &rhs)
+    {
+        int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+        value = normalize(value * rhs.value - q * mod());
+        return *this;
+    }
+    template <typename U = T>
+    typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type &operator*=(const Modular &rhs)
+    {
+        value = normalize(value * rhs.value);
+        return *this;
+    }
+
+    Modular &operator/=(const Modular &other) { return *this *= Modular(inverse(other.value, mod())); }
+
+    template <typename U>
+    friend bool operator==(const Modular<U> &lhs, const Modular<U> &rhs);
+
+    template <typename U>
+    friend bool operator<(const Modular<U> &lhs, const Modular<U> &rhs);
+
+    template <typename U>
+    friend std::istream &operator>>(std::istream &stream, Modular<U> &number);
+
+private:
+    Type value;
+};
+
+template <typename T>
+bool operator==(const Modular<T> &lhs, const Modular<T> &rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U>
+bool operator==(const Modular<T> &lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U>
+bool operator==(U lhs, const Modular<T> &rhs) { return Modular<T>(lhs) == rhs; }
+
+template <typename T>
+bool operator!=(const Modular<T> &lhs, const Modular<T> &rhs) { return !(lhs == rhs); }
+template <typename T, typename U>
+bool operator!=(const Modular<T> &lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U>
+bool operator!=(U lhs, const Modular<T> &rhs) { return !(lhs == rhs); }
+
+template <typename T>
+bool operator<(const Modular<T> &lhs, const Modular<T> &rhs) { return lhs.value < rhs.value; }
+
+template <typename T>
+Modular<T> operator+(const Modular<T> &lhs, const Modular<T> &rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U>
+Modular<T> operator+(const Modular<T> &lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U>
+Modular<T> operator+(U lhs, const Modular<T> &rhs) { return Modular<T>(lhs) += rhs; }
+
+template <typename T>
+Modular<T> operator-(const Modular<T> &lhs, const Modular<T> &rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U>
+Modular<T> operator-(const Modular<T> &lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U>
+Modular<T> operator-(U lhs, const Modular<T> &rhs) { return Modular<T>(lhs) -= rhs; }
+
+template <typename T>
+Modular<T> operator*(const Modular<T> &lhs, const Modular<T> &rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U>
+Modular<T> operator*(const Modular<T> &lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U>
+Modular<T> operator*(U lhs, const Modular<T> &rhs) { return Modular<T>(lhs) *= rhs; }
+
+template <typename T>
+Modular<T> operator/(const Modular<T> &lhs, const Modular<T> &rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U>
+Modular<T> operator/(const Modular<T> &lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U>
+Modular<T> operator/(U lhs, const Modular<T> &rhs) { return Modular<T>(lhs) /= rhs; }
+
+template <typename T, typename U>
+Modular<T> power(const Modular<T> &a, const U &b)
+{
+    assert(b >= 0);
+    Modular<T> x = a, res = 1;
+    U p = b;
+    while (p > 0)
+    {
+        if (p & 1)
+            res *= x;
+        x *= x;
+        p >>= 1;
     }
     return res;
 }
 
-int modinv(int x, int m = MOD)
+template <typename T>
+string to_string(const Modular<T> &number)
 {
-    return modpow(x, m - 2, m);
+    return to_string(number());
 }
 
-/*--------------- Seive -----------------------*/
-// Get All The Divisors Of That Number
-vector<int> getdiv(int n)
+template <typename T>
+std::ostream &operator<<(std::ostream &stream, const Modular<T> &number)
 {
-    vector<int> ans;
-    for (int i = 1; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            ans.pb(i);
-            ans.pb(n / i);
-        }
-    }
-    return ans;
-}
-// to get the prime factors of that number
-vector<int> getprimefac(int n)
-{
-    vector<int> ans;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            while (n % i == 0)
-            {
-                n /= i;
-                ans.pb(i);
-            }
-        }
-    }
-    return ans;
-}
-// get instant prime
-vector<ll> sieve(int n)
-{
-    int *arr = new int[n + 1]();
-    vector<ll> vect;
-    for (int i = 2; i <= n; i++)
-        if (arr[i] == 0)
-        {
-            vect.push_back(i);
-            for (int j = i * i; j <= n; j += i)
-                arr[j] = 1;
-        }
-    return vect;
-}
-// to invert a binary string
-void invert(string &s)
-{
-    int n = sz(s);
-    for (int i = 0; i < n; i++)
-    {
-        s[i] ^= '0' ^ '1';
-    }
+    return stream << number();
 }
 
-// display a vector
-void display(vector<int> a)
+template <typename T>
+std::istream &operator>>(std::istream &stream, Modular<T> &number)
 {
-    cout << "Displaying Vector:" << endl;
-    cout << a << endl;
+    typename common_type<typename Modular<T>::Type, int64_t>::type x;
+    stream >> x;
+    number.value = Modular<T>::normalize(x);
+    return stream;
 }
 
-// Flags to use: -std=c++17 -O2 -DLOCAL_PROJECT -Wshadow -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined
-/*/-----------------------------Code begins----------------------------------/*/
-void solve(int testcase)
+/*
+using ModType = int;
+
+struct VarMod { static ModType value; };
+ModType VarMod::value;
+ModType& md = VarMod::value;
+using Mint = Modular<VarMod>;
+*/
+
+constexpr int md = 998244353;
+using mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+int main()
 {
-    // kickstart(testcase);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int n;
     cin >> n;
-    if (n >= 60)
+    vector<int> p(n);
+    vector<vector<int>> g(n);
+    for (int i = 1; i < n; i++)
     {
-        cout << 22;
-        n %= 60;
+        cin >> p[i];
+        p[i]--;
+        g[p[i]].push_back(i);
     }
-    else
+    vector<vector<Mint>> dp(n, vector<Mint>(3, 0));
+    vector<Mint> aux(3, 0);
+    for (int i = n - 1; i >= 0; i--)
     {
-        cout << 21;
+        if (g[i].empty())
+        {
+            dp[i][2] = 1;
+        }
+        else
+        {
+            dp[i][0] = 1;
+            for (int j : g[i])
+            {
+                aux[0] = aux[1] = aux[2] = 0;
+                aux[0] += dp[i][0] * dp[j][0];
+                aux[1] += dp[i][0] * dp[j][1];
+                aux[0] += dp[i][0] * dp[j][2];
+                aux[1] += dp[i][0] * dp[j][2];
+                aux[1] += dp[i][1] * dp[j][0];
+                aux[2] += dp[i][1] * dp[j][1];
+                aux[1] += dp[i][1] * dp[j][2];
+                aux[2] += dp[i][1] * dp[j][2];
+                aux[2] += dp[i][2] * dp[j][0];
+                aux[2] += dp[i][2] * dp[j][1];
+                aux[2] += dp[i][2] * dp[j][2];
+                aux[2] += dp[i][2] * dp[j][2];
+                swap(dp[i], aux);
+            }
+        }
+        debug(i, dp[i]);
     }
-    cout << ":";
-    if (n < 10)
-    {
-        cout << 0;
-    }
-    cout << n << endl;
-}
-/* stuff you should look for
- * sbse pehle question dobara padho sir
- * Lower_bound  -->Lower_Bound of X ---> element >= x in the set
- * Upper_bound  -->Upper_Bound of X ---> element > x in the set
- * lambda fn is made using {auto name_of_the_function = [&](jo pass krna hai wo){fn body};
- * when u are not able to decide which one to remove then the answer is simply iterate and find the max/min answer for each index
- * if u r multiplying and u have to find equal multipy then u can take 1st and last everytime
- * if u have to make array increasing by adding 1 to subarray then sum of diffrences(which have to increased) is the answer
- * nlog(log(n)) bhi soch lo sir like jha multiples ka case aya wha pe seive of erathosthenisis ka concept lga do
- * cool hoja bsdk
- * jha 1 bdi value aur 1 choti value chahiye wha 2 pointer lga do sir
-    aur choti value wale pointer ko increase krte rehna jb testcase na meet ho to
- * 0 0 n  try kr lo if multiple ans ka case hai to ya isse similar kuch hai to
- * i>j wale sare chahiye to prefix wala lga do sir jisme curr ko calc kro prev se and fir usko map kr do
- * a+b = a^b + 2*a&b
- * a+b = a|b + a&b
- * special cases (n=1?)/ odd/even index
- * sir square wala bhi soch lo
- * consecutive elements ka scene hai to genedy apraoch lga do sir
- * follow the basics koi nya try kr rha hai toh uske primitive try kr
- * XOR --> ALWAYS TRY 45132
- */
-int32_t main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cout << fixed << setprecision(25);
-    cerr << fixed << setprecision(10);
-    auto start = std::chrono::high_resolution_clock::now();
-    int n = 1;
-    // cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        solve(i);
-    }
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
-    // cerr << "Time taken : " << ((long double)duration.count())/((long double) 1e9) <<"s "<< endl;
+    cout << (dp[0][0] + dp[0][2]) << '\n';
     return 0;
 }
