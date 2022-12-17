7ddb7e61507dacd462dfc7fa88e37343a06cd7aa
AMBUJ KUMAR <90747841+fantom787@users.noreply.github.com>
2022-12-17 11:48:55 +0530

    17th dec 2022

--- Web Dev/New Web Dev/wikiApi/app.js
+++ Web Dev/New Web Dev/wikiApi/app.js
@@ -9,6 +9,41 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wikiDB", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connection Sucessfull..."))
+  .catch((err) => console.log(err));
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
+
+app.post("/articles", function (req, res) {
+  const newar = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newar.save(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else{
+      console.log("Sucessfully Saved!");
+    }
+  });
+  res.send("hi this is post 1");
+});
+
+app.get("/article", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    res.send(foundArticles);
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server is At port 3000");
 });
